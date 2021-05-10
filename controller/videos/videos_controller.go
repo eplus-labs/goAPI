7f4cf342157ca30e3db9f9857a9727de8070253b
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-05-10 15:53:24 -0400

    cors update

--- controller/videos/videos_controller.go
+++ controller/videos/videos_controller.go
@@ -46,10 +46,10 @@ func Get(c *gin.Context) {
 
 	// videoMarshalled, _ := json.Marshal(videoStruct)
 
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+	// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
+	// c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 	c.JSON(http.StatusOK, string(videoMarshalled))
 
