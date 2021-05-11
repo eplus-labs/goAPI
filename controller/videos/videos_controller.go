5ddea15c979c9daf038d443035fb10834ccbcde8
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-05-11 09:23:41 -0400

    adding cors back in

--- controller/videos/videos_controller.go
+++ controller/videos/videos_controller.go
@@ -22,9 +22,9 @@ func Create(c *gin.Context) {
 	videoMarshalled, _ := json.Marshal(videoStruct)
 
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 	c.JSON(http.StatusCreated, string(videoMarshalled))
 
