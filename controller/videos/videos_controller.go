8a6d1bd5d3b5d2e1c863c382c5605a7010093d6f
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-05-11 11:50:48 -0400

    change to cors policy

--- controller/videos/videos_controller.go
+++ controller/videos/videos_controller.go
@@ -21,7 +21,7 @@ func Create(c *gin.Context) {
 
 	videoMarshalled, _ := json.Marshal(videoStruct)
 
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://18.208.179.111:8080/postvideo")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
