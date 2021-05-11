4bdd6930336943e792a0f0f1a9cbcf7e5d3ad394
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-05-11 12:08:38 -0400

    change to cors policy

--- controller/videos/videos_controller.go
+++ controller/videos/videos_controller.go
@@ -24,7 +24,7 @@ func Create(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://3.93.43.122:3000")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS, PUT, DELETE")
 
 	c.JSON(http.StatusCreated, string(videoMarshalled))
 
